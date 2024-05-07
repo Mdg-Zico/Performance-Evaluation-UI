aa49b589316598717f4a7842c10da7278f567b6e
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-05-07 16:28:30 +0100

    Implement form switching for staggered form

--- src/assets/js/form_repeater.js
+++ src/assets/js/form_repeater.js
@@ -1,11 +1,14 @@
-$(function () {
+$(document).ready(
+  function () {
+  let formsList = [];
   "use strict";
   $('.repeater').repeater({
     initEmpty: true,
     show: function () {
       $(this).slideDown();
-      $(this).addClass('d-none');
-      $(this).addClass('goal_'+$('.repeater').repeaterVal()['group-a'].length);
+      $(this).addClass('d-none goal_'+$('.repeater').repeaterVal()['group-a'].length);
+      formsList.push($(this));
+      console.log("This is formsList\n"+formsList);
       appendtoNav();
     },
     hide: function (deleteElement) {
@@ -16,9 +19,9 @@ $(function () {
   })
 
   function appendtoNav() {
-    console.log($('.repeater').repeaterVal()['group-a'])
+    // console.log($('.repeater').repeaterVal()['group-a'])
     const navLength = Object.keys($('.repeater').repeaterVal()['group-a']).length;
-    console.log(navLength);
+    // console.log(navLength);
     const navbar = $('.nav-tabs');
     navbar.append('<li class="nav-item goal_'+navLength+'" style="cursor: pointer;"><span class="nav-link">Goal '+navLength+'</span></li>')
   };
@@ -29,8 +32,27 @@ $(function () {
     $('li.'+goalNumber).remove();
   };
 
-  $('li.nav-item').on('click', function () {
+  $('.nav-tabs').on('click', '.nav-item', function () {
     const classList = $(this).attr("class").split(" ");
     const goalNumber = classList[classList.length - 1];
+    console.log("You have clicked on navtab "+goalNumber);
+    const navTabs = $('nav-tabs').children();
+    console.log(navTabs);
+    navTabs.map(item => {
+      if (!item.hasClass(goalNumber) && item.hasClass('active_link')) {
+        item.removeClass('active_link');
+      }
+    });
+    formsList.map(item => {
+      if (!item.hasClass('d-none') && !item.hasClass(goalNumber)) {
+        console.log(item);
+        item.addClass('d-none');
+      }
+      if (item.hasClass(goalNumber) && item.hasClass('d-none')) {
+        console.log(item);
+        item.removeClass('d-none');
+      }
+    })
+    $(this).addClass('active_link');
   })
 });
