24e478d5077df0fa56aaa54db32dad48f8b03512
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-05-14 13:14:06 +0100

    Implement highlighting current month on page load

--- src/assets/js/performance-diary.js
+++ src/assets/js/performance-diary.js
@@ -1,5 +1,9 @@
 $(document).ready(
   function () {
+    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+    const date = new Date();
+    currentMonth = months[date.getMonth()];
+    const navtab = $('.nav-btn').attr()
     $('.nav2').on('click', '.nav-btn', function () {
       if ($(this).hasClass('active-navbtn')) {
         pass;
