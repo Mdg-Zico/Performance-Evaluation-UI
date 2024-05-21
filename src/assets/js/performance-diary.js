78c147502734ebc1760f366a8d6646a88e3f3444
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-05-21 10:21:55 +0100

    Edit month list, leave code stubs for future functions

--- src/assets/js/performance-diary.js
+++ src/assets/js/performance-diary.js
@@ -1,6 +1,19 @@
 $(document).ready(
   function () {
-    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ]
     const date = new Date();
     currentMonth = months[date.getMonth()];
     console.log(currentMonth);
@@ -10,11 +23,23 @@ $(document).ready(
     $('.nav2').on('click', '.nav-btn', function () {
       if ($(this).hasClass('active-navbtn')) {
         pass;
+
       } else {
         $('.nav-btn.active-navbtn').removeClass('active-navbtn');
         console.log("Clicked nav element"+$(this).attr('name'));
         $(this).addClass('active-navbtn');
+        getPerformanceScores();
       }
     })
   }
 )
+
+function getPerformanceScores(){
+  $ajax({
+    // get scores from backend
+  })
+}
+
+function displayGoalsAndPerformanceScores(response) {
+
+}
